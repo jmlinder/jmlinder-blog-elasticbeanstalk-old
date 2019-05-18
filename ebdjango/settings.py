c90a0d43a62858cb07499764992d15cb1b62a4ca
Joseph Linder <joemlinder@gmail.com>
2019-05-17 21:07:41 -0500

    updating gitignore

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'qa_%u2*#v&7g=oxf-sd^6t@*uhp@g!1eilmln)*spe!ic)a@cs'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'mysite-dev.xa37kgm4mq.us-east-1.elasticbeanstalk.com',
+    'jmlinder-dev.xa37kgm4mq.us-east-1.elasticbeanstalk.com',
     'blog.jmlinder.com',
     'jmlinder.com',
     'localhost'
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'ebdjango.wsgi.application'
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
@@ -90,15 +90,6 @@ if 'RDS_HOSTNAME' in os.environ:
             'PORT': os.environ['RDS_PORT'],
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
