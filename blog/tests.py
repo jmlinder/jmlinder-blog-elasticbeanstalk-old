0b54603f72940b565e610fb95d8f2222110e4595
Joseph Linder <joemlinder@gmail.com>
2019-05-31 20:04:27 -0500

    travis configuration

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+settings.configure()
+
+c = Client()
+
+response = c.get('/')
+response.status_code
+
+
+class Mytest(Client):
+    def test_detail(self):
+        response = self.client.get('/blog/post_list.html')
+        print(response['location'])
+        self.assertEqual(response.status_code, 200)
