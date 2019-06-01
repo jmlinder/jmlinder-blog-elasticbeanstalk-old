89a2bd6db985a5ac939fadd4863e5bae5c6f083f
Joseph Linder <joemlinder@gmail.com>
2019-05-31 20:12:49 -0500

    writing travis tests

--- blog/tests.py
+++ blog/tests.py
@@ -3,14 +3,8 @@ from django.test import Client
 
 # Create your tests here.
 
-c = Client()
 
-response = c.get('/')
-response.status_code
-
-
-class Mytest(Client):
-    def test_detail(self):
-        response = self.client.get('/blog/post_list.html')
-        print(response['location'])
-        self.assertEqual(response.status_code, 200)
+def test_detail(self):
+    response = self.client.get('/blog/post_list.html')
+    print(response['location'])
+    self.assertEqual(response.status_code, 200)
